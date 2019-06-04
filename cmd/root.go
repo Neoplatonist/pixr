a0ff7e88ac60a057c2144c369cbfe9de62763d39
Neoplatonist <mr.neoplatonist@gmail.com>
2019-06-05 00:40:28 +0900

    added root cmd flags; set cli flags > config when choosing settings; autogens db credentials and sets server port

--- cmd/root.go
+++ cmd/root.go
@@ -1,9 +1,18 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
 
 var (
-	rootOptions struct{}
+	rootOptions struct {
+		databaseCred string
+		serverPort   string
+	}
 
 	rootCmd = &cobra.Command{
 		Use:   "pixr",
@@ -14,9 +23,58 @@ var (
 )
 
 func init() {
-	// root command cli flags go here
+	rootCmd.Flags().StringVarP(
+		&rootOptions.databaseCred,
+		"database",
+		"d",
+		"",
+		"username:password@tcp(location:port)/dbname",
+	)
+
+	rootCmd.Flags().StringVarP(
+		&rootOptions.serverPort,
+		"port",
+		"p",
+		"",
+		"server port",
+	)
 }
 
 func rootCmdFunc(cmd *cobra.Command, args []string) error {
+	username := viper.GetString("database.username")
+	password := viper.GetString("database.password")
+	location := viper.GetString("database.location")
+	dbPort := viper.GetString("database.port")
+	dbname := viper.GetString("database.dbname")
+
+	var dbCred string
+	if rootOptions.databaseCred != "" {
+		dbCred = rootOptions.databaseCred + "?charset=utf8&parseTime=True"
+	} else if username != "" &&
+		password != "" &&
+		location != "" &&
+		dbPort != "" &&
+		dbname != "" {
+
+		dbCred = fmt.Sprintf(
+			"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True",
+			username,
+			password,
+			location,
+			dbPort,
+			dbname,
+		)
+	} else {
+		return errors.New("no database credentials found")
+	}
+
+	port := ":" + rootOptions.serverPort
+	if port == ":" {
+		port = ":" + viper.GetString("server.port")
+	}
+
+	logger.Printf("dbCred: %s", dbCred)
+	logger.Printf("port: %s", port)
+
 	return nil
 }
