ad95f7588920600577bbf6dc7a7358995c36d601
Neoplatonist <mr.neoplatonist@gmail.com>
2019-06-04 17:49:14 +0900

    created upload page base template and event handlers

--- frontend/src/routes/Upload.svelte
+++ frontend/src/routes/Upload.svelte
@@ -1,9 +1,64 @@
 <script>
-  // your script goes here
+  import { onMount, onDestroy } from 'svelte'
+  import {store } from '../store'
+
+  let active,
+      dropzone,
+      inputFiles,
+      selectButton,
+      unsubscribe
+
+  onMount(() => {
+    unsubscribe = store.subscribe(data => {
+
+    })
+  })
+
+  // Drag Events
+  const handleDragEnter = e => {}
+  const handleDragOver = e => {}
+  const handleDrageLeave = e => {}
+  const handleDrop = e => {}
+
+  // Form Button Selection
+  const handleInput = e => {}
+
+  onDestroy(() => unsubscribe())
 </script>
 
 <style>
   /* your styles go here */
 </style>
 
-<h2>Upload Page</h2>
+<section
+  class:active
+  bind:this={dropzone}
+  on:dragenter={handleDragEnter}
+  on:dragover={handleDragOver}
+  on:dragleave={handleDrageLeave}
+  on:drop={handleDrop}
+  >
+  <form>
+    <p>Click and drag files or use the button.</p>
+
+    <input
+      id="fileElem"
+      type="file"
+      accept="image/*"
+      bind:this={inputFiles}
+      on:change={handleInput}
+      multiple>
+
+    <section>
+      <label
+        bind:this={selectButton}
+        for="fileElem">Select Files</label>
+
+      <!-- upload button -->
+    </section>
+  </form>
+
+  <!-- loader -->
+
+  <!-- gallery -->
+</section>
