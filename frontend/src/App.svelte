3e0e2cef6c5086dcc9a71591f5b2d83464f04bf1
Neoplatonist <mr.neoplatonist@gmail.com>
2019-06-04 15:58:17 +0900

    added svelte-routing and .gitignore

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,11 +1,22 @@
 <script>
-	export let name;
+  import { Router, links, Route } from 'svelte-routing'
+  import Home from './routes/Home.svelte'
+
+  export let url = ""
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
+<style></style>
 
-<h1>Hello {name}!</h1>
+<Router url='{url}'>
+  <header use:links>
+    <a href="/">Home</a>
+  </header>
+
+  <main>
+    <Route path="/" component={Home} />
+  </main>
+
+  <footer>
+    <p>All Rights Reserved. 2019 &copy; made with &hearts; by <a href="https://joshfjohnston.com">Josh Johnston</a>.</p>
+  </footer>
+</Router>
